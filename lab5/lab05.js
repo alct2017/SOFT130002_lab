18f1c2b9506df66efda5f226b3708d23287c3cc7
Alct2017 <imjliu@outlook.com>
2020-04-26 19:44:26 +0800

    finish

--- lab5/lab05.js
+++ lab5/lab05.js
@@ -8,9 +8,17 @@
 let url = document.getElementById("url");
 let url_submit = document.getElementById("url_submit");
 let url_result = document.getElementById("url-result");
-url_submit.addEventListener('click',showWindowHref);
-function showWindowHref(){
-
+url_submit.addEventListener('click', showWindowHref);
+function showWindowHref() {
+    const nameRegExp = /name=[^&]*/;
+    let name = nameRegExp.exec(url.value);
+    if (name == null) {
+        name = "No name";
+    }
+    else {
+        name = name[0].substring(5);
+    }
+    url_result.value = name;
 }
 //2. 每隔五秒运行一次函数直到某一整分钟停止，比如从20:55:45运行到20:56:00停止；或者运行10次，先到的为准。从1开始每过五秒，输入框内数值翻倍。初始值为1。
 //注意：你可以在函数 timeTest内部 和 timeTest外部 写代码使得该功能实现。
@@ -18,7 +26,21 @@ function showWindowHref(){
 
 //提示：mul为html中id为"mul"的元素对象，可直接通过mul.value获得其内的输入值。
 let mul = document.getElementById("mul");
-function timeTest(){
+let counter = 0,
+    onloadMinute,
+    mulIntervId;
+const maxTimes = 10;
+window.onload = function () {
+    mul.value = 1;
+    onloadMinute = (new Date()).getMinutes();
+    mulIntervId = setInterval(timeTest, 5000);
+};
+function timeTest() {
+    mul.value *= 2;
+    counter++;
+    if ((new Date()).getMinutes() != onloadMinute || counter >= maxTimes) {
+        clearInterval(mulIntervId);
+    }
 }
 //3. 判断输入框most里出现最多的字符，并统计出来。统计出是信息在most_result输入框内以"The most character is:" + index + " times:" + max的形式显示。
 //如果多个出现数量一样则选择一个即可。
@@ -28,7 +50,24 @@ function timeTest(){
 let most = document.getElementById("most");
 let result = document.getElementById("most-result");
 let most_submit = document.getElementById("most_submit");
-most_submit.addEventListener('click',arrSameStr);
-function arrSameStr(){
-
+most_submit.addEventListener('click', arrSameStr);
+function arrSameStr() {
+    const str = most.value;
+    let dict = new Array();
+    for (let char of str) {
+        if (char in dict) {
+            dict[char] += 1;
+        }
+        else {
+            dict[char] = 1;
+        }
+    }
+    let max = 0, maxKey;
+    for (let key in dict) {
+        if (dict[key] > max) {
+            max = dict[key];
+            maxKey = key;
+        }
+    }
+    result.value = "The most character is:" + maxKey + " times:" + max;
 }
